6203e6bac699ef3764f948f67211442f83490ca6
forest93 <forest93@163.com>
2019-05-25 16:12:48 +0800

    Upgrade package versions. Add docs.

--- WikibaseClientLite.ModuleExporter/ObjectModel/ILuaModule.cs
+++ WikibaseClientLite.ModuleExporter/ObjectModel/ILuaModule.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace WikibaseClientLite.ModuleExporter.ObjectModel
 {
+
+    /// <summary>
+    /// Represents a LUA module, either on disk or on MediaWiki site.
+    /// </summary>
     public interface ILuaModule : IDisposable
     {
+
+        /// <summary>
+        /// Gets the writer that can be used to write module content.
+        /// </summary>
+        /// <remarks>
+        /// Usually this property returns the same instance for the same <see cref="ILuaModule"/>.
+        /// Do not close the returned <see cref="TextWriter"/>.
+        /// </remarks>
         TextWriter Writer { get; }
 
+        /// <summary>
+        /// Closes <see cref="Writer"/> and submits the module.
+        /// </summary>
+        /// <param name="editSummary">The edit summary.</param>
         Task SubmitAsync(string editSummary);
 
     }
